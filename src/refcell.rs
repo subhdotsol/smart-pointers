857cfe5b84870e7d46c8648edcd3bcc15cd73676
subh <subhajitchaudhury05@gmail.com>
2026-03-27 23:07:41 +0530

    refcell: implement RefCell::new

--- src/refcell.rs
+++ src/refcell.rs
@@ -12,3 +12,12 @@ pub struct RefCell<T> {
     value: UnsafeCell<T>,
     state: Cell<RefState>,
 }
+
+impl<T> RefCell<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            value: UnsafeCell::new(value),
+            state: Cell::new(RefState::Unshared),
+        }
+    }
+}
