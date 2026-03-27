5f15ab03e3bd4dc0c557e4218cd812d347741ced
subh <subhajitchaudhury05@gmail.com>
2026-03-27 23:06:28 +0530

    cell: implement Cell::new

--- src/cell.rs
+++ src/cell.rs
@@ -3,3 +3,11 @@ use std::cell::UnsafeCell;
 pub struct Cell<T> {
     value: UnsafeCell<T>,
 }
+
+impl<T> Cell<T> {
+    pub fn new(value: T) -> Self {
+        Cell {
+            value: UnsafeCell::new(value),
+        }
+    }
+}
