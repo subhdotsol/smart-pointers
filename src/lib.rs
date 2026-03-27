65ff7bc769b56e70438da47a9fe6421c562f15fd
subh <subhajitchaudhury05@gmail.com>
2026-03-27 23:09:52 +0530

    lib: consolidate all implementations into a single file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,188 @@
-pub mod cell;
-pub mod rc;
-pub mod refcell;
+//! A collection of simple smart pointers and cells.
+
+use std::cell::UnsafeCell;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+// --- Cell ---
+
+pub struct Cell<T> {
+    value: UnsafeCell<T>,
+}
+
+impl<T> Cell<T> {
+    pub fn new(value: T) -> Self {
+        Cell {
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn set(&self, value: T) {
+        unsafe {
+            *self.value.get() = value;
+        }
+    }
+
+    pub fn get(&self) -> T
+    where
+        T: Copy,
+    {
+        unsafe { *self.value.get() }
+    }
+}
+
+// --- Rc ---
+
+struct RcInner<T> {
+    value: T,
+    refcount: Cell<usize>,
+}
+
+pub struct Rc<T> {
+    inner: NonNull<RcInner<T>>,
+    _marker: PhantomData<RcInner<T>>,
+}
+
+impl<T> Rc<T> {
+    pub fn new(v: T) -> Self {
+        let inner = Box::new(RcInner {
+            value: v,
+            refcount: Cell::new(1),
+        });
+
+        Rc {
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> std::ops::Deref for Rc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &unsafe { self.inner.as_ref() }.value
+    }
+}
+
+impl<T> Clone for Rc<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.refcount.get();
+        inner.refcount.set(c + 1);
+        Rc {
+            inner: self.inner,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> Drop for Rc<T> {
+    fn drop(&mut self) {
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.refcount.get();
+        if c == 1 {
+            let _ = unsafe { Box::from_raw(self.inner.as_ptr()) };
+        } else {
+            inner.refcount.set(c - 1);
+        }
+    }
+}
+
+// --- RefCell ---
+
+#[derive(Copy, Clone)]
+enum RefState {
+    Unshared,
+    Shared(usize),
+    Exclusive,
+}
+
+pub struct RefCell<T> {
+    value: UnsafeCell<T>,
+    state: Cell<RefState>,
+}
+
+impl<T> RefCell<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            value: UnsafeCell::new(value),
+            state: Cell::new(RefState::Unshared),
+        }
+    }
+
+    pub fn borrow(&self) -> Option<Ref<'_, T>> {
+        match self.state.get() {
+            RefState::Unshared => {
+                self.state.set(RefState::Shared(1));
+                Some(Ref { refcell: self })
+            }
+            RefState::Shared(n) => {
+                self.state.set(RefState::Shared(n + 1));
+                Some(Ref { refcell: self })
+            }
+            RefState::Exclusive => None,
+        }
+    }
+
+    pub fn borrow_mut(&self) -> Option<RefMut<'_, T>> {
+        if let RefState::Unshared = self.state.get() {
+            self.state.set(RefState::Exclusive);
+            Some(RefMut { refcell: self })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Ref<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+impl<T> std::ops::Deref for Ref<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Exclusive | RefState::Unshared => unreachable!(),
+            RefState::Shared(1) => {
+                self.refcell.state.set(RefState::Unshared);
+            }
+            RefState::Shared(n) => {
+                self.refcell.state.set(RefState::Shared(n - 1));
+            }
+        }
+    }
+}
+
+pub struct RefMut<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+impl<T> std::ops::Deref for RefMut<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> std::ops::DerefMut for RefMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.refcell.value.get() }
+    }
+}
+
+impl<T> Drop for RefMut<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Shared(_) | RefState::Unshared => unreachable!(),
+            RefState::Exclusive => {
+                self.refcell.state.set(RefState::Unshared);
+            }
+        }
+    }
+}
