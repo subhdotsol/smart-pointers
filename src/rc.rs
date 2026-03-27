344a22a8c6f0bb817b6c41072fda6eaef3df9dcb
subh <subhajitchaudhury05@gmail.com>
2026-03-27 23:06:57 +0530

    rc: implement Rc::new

--- src/rc.rs
+++ src/rc.rs
@@ -11,3 +11,18 @@ pub struct Rc<T> {
     inner: NonNull<RcInner<T>>,
     _marker: PhantomData<RcInner<T>>,
 }
+
+impl<T> Rc<T> {
+    pub fn new(v: T) -> Self {
+        let inner = Box::new(RcInner {
+            value: v,
+            refcount: Cell::new(1),
+        });
+
+        Rc {
+            // SAFETY: Box does not give us a null pointer.
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
+            _marker: PhantomData,
+        }
+    }
+}
