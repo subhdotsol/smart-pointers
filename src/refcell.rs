e5e1988e59c3778c703448db3e2e45ba84c88cb1
subh <subhajitchaudhury05@gmail.com>
2026-03-27 23:07:41 +0530

    refcell: add RefCell struct definition

--- src/refcell.rs
+++ src/refcell.rs
@@ -1,6 +1,14 @@
+use crate::cell::Cell;
+use std::cell::UnsafeCell;
+
 #[derive(Copy, Clone)]
 enum RefState {
     Unshared,
     Shared(usize),
     Exclusive,
+}
+
+pub struct RefCell<T> {
+    value: UnsafeCell<T>,
+    state: Cell<RefState>,
 }
