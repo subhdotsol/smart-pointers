2fefb6343c0b4098944189508840e56cb8e5fbe7
subh <subhajitchaudhury05@gmail.com>
2026-03-27 23:06:57 +0530

    rc: add Rc struct definition

--- src/rc.rs
+++ src/rc.rs
@@ -1,6 +1,13 @@
 use crate::cell::Cell;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
 
 struct RcInner<T> {
     value: T,
     refcount: Cell<usize>,
+}
+
+pub struct Rc<T> {
+    inner: NonNull<RcInner<T>>,
+    _marker: PhantomData<RcInner<T>>,
 }
